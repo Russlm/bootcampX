8a1ffbb06a053995cb461770596e146ae8103498
Russell Mair <russell.e.mair@gmail.com>
2022-01-04 10:19:10 +0000

    fixes a copy paste error.

--- 1_queries/6_unlinked_github_students.sql
+++ 1_queries/6_unlinked_github_students.sql
@@ -4,17 +4,10 @@ WHERE github IS NULL AND end_date IS NOT NULL
 ORDER BY name 
 
 CREATE TABLE assignments (
-  id SERAL PRIMARY KEY NOT NULL,
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
-  content VARCHAR(255),
+  content TEXT,
   day INTEGER,
   chapter INTEGER,
   duration INTEGER,
-);
-CREATE TABLE assignment_submissions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration INTEGER,
-  submission_date DATE,
 );
