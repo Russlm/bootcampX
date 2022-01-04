d78d18fd6bfd79e5e05106a06f76ddce39a66a5c
Russell Mair <russell.e.mair@gmail.com>
2022-01-04 10:12:50 +0000

    adds the assignments and assignment_submissions tables.

--- 1_queries/6_unlinked_github_students.sql
+++ 1_queries/6_unlinked_github_students.sql
@@ -2,3 +2,19 @@ SELECT name, email, phone
 FROM students
 WHERE github IS NULL AND end_date IS NOT NULL
 ORDER BY name 
+
+CREATE TABLE assignments (
+  id SERAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content VARCHAR(255),
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER,
+);
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE,
+);
